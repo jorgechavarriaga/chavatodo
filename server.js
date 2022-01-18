5812885a0054671830f165ea417c45c16d749885
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2022-01-18 16:30:59 -0500

    2

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<todo-app>'));
+app.use(express.static(__dirname + '/dist/<chavatodo>'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<todo-app>/index.html'));});
+'/dist/<chavatodo>/index.html'));});
 app.listen(process.env.PORT || 8080);
