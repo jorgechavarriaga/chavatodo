d0bb0ae96b34dde28e17b548e4923ab8f35ec668
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2022-01-18 16:52:04 -0500

    2

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
-
-    //Install express server
-    const express = require('express');
-    const path = require('path');
-
-    const app = express();
-
-    // Serve only the static files form the dist directory
-    app.use(express.static(__dirname + '/dist/chavatodo'));
-
-    app.get('/*', function(req,res) {
-        
-    res.sendFile(path.join(__dirname+'/dist/chavatodo/index.html'));
-    });
-
-    // Start the app by listening on the default Heroku port
-    app.listen(process.env.PORT || 8080);
+const express = require('express');
+const app = express();
+ 
+/**
+ * function to redirect traffic from http to https
+ */
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+app.use(requireHTTPS);
+/**
+ * used to serve our static file
+ */
+app.use(express.static('./dist/chavatodo'));
+ 
+/**
+ * used to redirect paths to index.html file in dist folder
+ */
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/chavatodo/'}
+  );
+  });
+/**
+ * listen to request at the port specified from env var PORT or default Heroku port 8080
+ */
+app.listen(process.env.PORT || 8080);
